df79a50d6da97f7503abface8ca8850078947c79
varughese <mat.varughese3@gmail.com>
2021-07-29 23:35:53 -0400

    update

--- app/server.go
+++ app/server.go
@@ -17,20 +17,25 @@ func main() {
 
 	for {
 		conn, err := l.Accept()
-		fmt.Println("new connection", conn)
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
+		fmt.Println("NEW CONN", conn)
 		go handleRequest(conn)
 	}
 }
 
 func handleRequest(conn net.Conn) {
 	for {
+		// ReadString reads the data up until a new line.
+		// If there is no new line, it sets err to io.EOF.
+		// In this case, then we close the connection.
 		data, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println("READING", data)
 		if err != nil {
 			if len(data) == 0 && err == io.EOF {
+				fmt.Println("CLOSING CONN", conn)
 				conn.Close()
 				break
 			}
