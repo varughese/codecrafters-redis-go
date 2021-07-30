4df1ca39b913c970bb224eec2019e9d9774fa91f
varughese <mat.varughese3@gmail.com>
2021-07-29 23:01:40 -0400

    send pong

--- app/server.go
+++ app/server.go
@@ -12,9 +12,26 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+
+	conn.Write([]byte("+PONG\r\n"))
+
+	conn.Close()
 }
