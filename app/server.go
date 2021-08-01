515a88f1326c14c3975b40c91589bcd824c8df57
varughese <mat.varughese3@gmail.com>
2021-08-01 18:29:41 -0400

    echo

--- app/server.go
+++ app/server.go
@@ -35,40 +35,49 @@ const (
 )
 
 type command struct {
-	id      COMMAND_ID
-	content []byte
+	id   COMMAND_ID
+	args []byte
 }
 
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
 	for {
-		msg, err := parseConnection(conn)
-		if len(msg) == 0 || err != nil {
-			if err == io.EOF {
-				fmt.Println("CLOSING CONN", conn)
-				break
-			}
-			fmt.Println("Error reading:", err.Error())
+		msg, cmd, err := parseConnection(conn)
+		fmt.Println(cmd)
+		if len(msg) == 0 || err == io.EOF {
+			fmt.Println("CLOSING CONN", conn)
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading:", err)
 			break
 		}
 
+		if cmd.id == ECHO {
+			start := []byte("$" + strconv.Itoa(len(msg)) + "\r\n")
+			res := append(append(start, msg...), []byte("\r\n")...)
+			conn.Write([]byte(res))
+		} else if cmd.id == PONG {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+
 		fmt.Println("READING", msg)
-		conn.Write([]byte("+PONG\r\n"))
 	}
 }
 
-func parseConnection(conn net.Conn) ([]byte, error) {
+func parseConnection(conn net.Conn) ([]byte, *command, error) {
 	reader := bufio.NewReader(conn)
 
 	return parseRedisDatatype(reader)
 }
 
-func parseRedisDatatype(reader *bufio.Reader) ([]byte, error) {
+func parseRedisDatatype(reader *bufio.Reader) ([]byte, *command, error) {
 	dataType, err := reader.ReadByte()
 	msg := []byte("")
+	cmd := command{id: PONG, args: nil}
 
 	if err != nil {
-		return []byte(""), err
+		return []byte(""), &cmd, err
 	}
 
 	msg, err = []byte(""), nil
@@ -84,36 +93,42 @@ func parseRedisDatatype(reader *bufio.Reader) ([]byte, error) {
 		stringByteLength, err := strconv.Atoi(string(l[:len(l)-2]))
 
 		if err != nil {
-			return msg, err
+			return msg, &cmd, err
 		}
 
 		msg = make([]byte, stringByteLength)
 		reader.Read(msg)
 	case "*":
-		l, _ := reader.ReadByte()
-		length, err := strconv.Atoi(string(l))
+		l, err := reader.ReadBytes('\n')
+		// trim off the \r\n
+		length, err := strconv.Atoi(string(l[:len(l)-2]))
 
 		if err != nil {
-			return msg, err
+			return msg, &cmd, err
 		}
 
 		for i := 0; i < length; i++ {
-			currentMsg, err := parseRedisDatatype(reader)
+			msg, _, err = parseRedisDatatype(reader)
 			if err != nil {
-				return msg, err
+				return msg, &cmd, err
 			}
-			fmt.Println(currentMsg)
+			if i == 1 {
+				cmd.id = ECHO
+			} else {
+				cmd.args = msg
+			}
+			fmt.Println(string(msg))
+			// read til next delimiter
+			reader.ReadBytes('\n')
 		}
-
-		msg, err = reader.ReadBytes('\n')
 	case "\r":
 	case "\n":
 		// If the byte is part of a CLRF, return empty
-		return msg, err
+		return msg, &cmd, err
 	default:
 		err = fmt.Errorf("Invalid start of response. Unknown data type: %s", string(dataType))
 	}
 
-	return msg, err
+	return msg, &cmd, err
 
 }
